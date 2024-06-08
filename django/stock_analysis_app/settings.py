91ec7078970bde375b3349e199639bcb3064dbaf
“TOKB27” <sa122moshita@gmail.com>
2024-06-09 01:51:26 +0900

    “Tailwindの追加”

--- django/stock_analysis_app/settings.py
+++ django/stock_analysis_app/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts',
+    # 'accounts',
 ]
 
 MIDDLEWARE = [
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'stock_analysis_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,10 +123,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = os.getenv('STATIC_URL')
-STATIC_ROOT = os.getenv('STATIC_ROOT')
-MEDIA_URL = os.getenv('MEDIA_URL')
-MEDIA_ROOT = os.getenv('MEDIA_ROOT')
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = os.getenv('STATIC_URL', '/static/')
+STATIC_ROOT = os.getenv('STATIC_ROOT', os.path.join(BASE_DIR, 'staticfiles'))
+
+# Media files
+MEDIA_URL = os.getenv('MEDIA_URL', '/media/')
+MEDIA_ROOT = os.getenv('MEDIA_ROOT', os.path.join(BASE_DIR, 'media'))
+
+# Additional locations of static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -134,18 +142,18 @@ MEDIA_ROOT = os.getenv('MEDIA_ROOT')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CustomUserモデルの設定
-AUTH_USER_MODEL = 'accounts.CustomUser'
+# AUTH_USER_MODEL = 'accounts.CustomUser'
 
 # ブラウザが閉じられたときにセッション終了
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
 # ログイン前後の遷移先
-LOGIN_URL = '/accounts/login/'
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/accounts/login/'
+# LOGIN_URL = '/accounts/login/'
+# LOGIN_REDIRECT_URL = '/'
+# LOGOUT_REDIRECT_URL = '/accounts/login/'
 
 # ログインしていなくても表示可能なページ
-PUBLIC_PATHS = [
-    '/accounts/login/',
-    '/auth/*',
-]
+# PUBLIC_PATHS = [
+#     '/accounts/login/',
+#     '/auth/*',
+# ]
