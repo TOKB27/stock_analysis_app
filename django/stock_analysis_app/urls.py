1789c6d8518387e46816f09a91f9e8fcfabac282
“TOKB27” <sa122moshita@gmail.com>
2024-06-30 21:54:15 +0900

    “ログイン後のダッシュボード遷移”

--- django/stock_analysis_app/urls.py
+++ django/stock_analysis_app/urls.py
@@ -5,5 +5,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('', TemplateView.as_view(template_name="base.html")),
+    path('', include('dashboard.urls')),
 ]
